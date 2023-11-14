214ed4e6a32b0ad9bf762c855cf0ddec1a50ea15
JoseRomero616 <jo.seeduardo25397stk@gmail.com>
2023-11-14 09:29:52 -0500

    Update app_sdk.py

--- app/app_sdk.py
+++ app/app_sdk.py
@@ -6,6 +6,12 @@ from sqlalchemy import create_engine, Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Importar Dynatrace OneAgent SDK
+import oneagent
+
+# Inicializar Dynatrace OneAgent SDK
+oneagent.initialize()
+
 # Obtener variables de entorno
 db_host = os.getenv('DBHOST', 'localhost')  # Default a 'localhost' si no está definida
 db_port = os.getenv('DBPORT', '3306')       # Default a '3306' si no está definida
@@ -27,31 +33,41 @@ Session = sessionmaker(bind=engine)
 # Tornado Request Handler para /ping
 class PingHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("pong")
+        with oneagent.get_sdk().trace_web_request(self.request) as tracer:
+            tracer.start()
+            self.write("pong")
+            tracer.end()
 
 # Tornado Request Handler para /pedido (POST)
 class PedidoHandler(tornado.web.RequestHandler):
     def post(self):
-        data = json.loads(self.request.body)
-        try:
-            numero = int(data.get('numero'))
-            session = Session()
-            nuevo_pedido = Pedido(numero=numero)
-            session.add(nuevo_pedido)
-            session.commit()
-            session.close()
-            self.write({"message": "registro agregado"})
-        except ValueError:
-            self.set_status(400)
-            self.write({"error": "Solo se aceptan números"})
+        with oneagent.get_sdk().trace_web_request(self.request) as tracer:
+            tracer.start()
+            try:
+                data = json.loads(self.request.body)
+                numero = int(data.get('numero'))
+                session = Session()
+                nuevo_pedido = Pedido(numero=numero)
+                session.add(nuevo_pedido)
+                session.commit()
+                session.close()
+                self.write({"message": "registro agregado"})
+            except ValueError:
+                self.set_status(400)
+                self.write({"error": "Solo se aceptan números"})
+                tracer.error("Error: Solo se aceptan números")
+            tracer.end()
 
 # Tornado Request Handler para /pedidos (GET)
 class PedidosHandler(tornado.web.RequestHandler):
     def get(self):
-        session = Session()
-        pedidos = session.query(Pedido).all()
-        session.close()
-        self.write({"pedidos": [{"id": pedido.id, "numero": pedido.numero} for pedido in pedidos]})
+        with oneagent.get_sdk().trace_web_request(self.request) as tracer:
+            tracer.start()
+            session = Session()
+            pedidos = session.query(Pedido).all()
+            session.close()
+            self.write({"pedidos": [{"id": pedido.id, "numero": pedido.numero} for pedido in pedidos]})
+            tracer.end()
 
 # Crear aplicación Tornado y definir rutas
 def make_app():
@@ -67,3 +83,6 @@ if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
+
+    # Apagar el SDK de Dynatrace antes de finalizar la aplicación
+    oneagent.shutdown()
